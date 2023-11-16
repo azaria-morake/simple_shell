4f4ca84479bd7b1f92ab9241e2b648817533b6f5
azaria-morake <141196419+azaria-morake@users.noreply.github.com>
2023-11-16 02:12:40 +0200

    Add files via upload

---
+++ bets.c
@@ -0,0 +1,18 @@
+/**
+ * main - This is the main function.
+ * @void: Takes no arguments.
+ *
+ * Description: This is the main function where all the work happens.
+ * Return: Returns 0 for success.
+ */
+
+#include <stdio.h>
+
+int main(void)
+
+{
+	printf("This is the beginning of simple_shell.\n");
+
+	return (0);
+
+}
