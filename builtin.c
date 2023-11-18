54f0e415edfcc12d45c00abc78a991efd6195680
azaria-morake <azaria-morake@gmail.com>
2023-11-18 00:28:07 -0800

    update files

--- builtin.c
+++ builtin.c
@@ -92,6 +92,6 @@ int _myhelp(info_t *info)
 	arg_array = info->argv;
 	_puts("help call works. Function not yet implemented \n");
 	if (0)
-		_puts(*arg_array); 
+		_puts(*arg_array);
 	return (0);
 }
